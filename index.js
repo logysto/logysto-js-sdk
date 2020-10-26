11171d864c0a069423a256f843678cbf29c36c7f
Wilmer Alzate Roman <wilmeralzateroman@gmail.com>
2020-10-26 11:10:20 -0500

    add traceability function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use-stric'
-const { config, getPriceFromLocations, searchAddress } =require("./src/controllers/logysto-sdk");
+const { config, getPriceFromLocations, searchAddress, getTraceabilityByCode } =require("./src/controllers/logysto-sdk");
 exports.config = config;
 exports.getPrice = getPriceFromLocations;
 exports.searchAddress = searchAddress;
+exports.getTraceabilityByCode = getTraceabilityByCode;
